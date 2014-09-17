b2a6590b41ac1ad9167af82165930a42c8caf64f
svitali <svitali@amazon.com>
2014-09-17 13:16:03 +0100

    yea

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.sendfile('/srv/www/app1/shared/config/app_data.yml');
+app.get('/', function(request, response) {
+//logger.info('Console and File logging with Winston! Request from ' + req.ip + ' at ' + new Date() );
+response.send('Server: ' + os.hostname() + '<BR><BR>From: ' + request.ip + '<BR><BR>At: ' + new Date());
 });
 
 app.use(express.static('public'));
 
-app.listen(80);
+app.listen(80,'subdomain.domain.com');
 console.log('Listening on port 80');
