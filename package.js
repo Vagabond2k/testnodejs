12b2447f726b3f018d426371d90054a6bca6d0fa
svitali <svitali@amazon.com>
2014-09-17 13:23:31 +0100

    A

---
+++ package.js
@@ -0,0 +1,12 @@
+
+{
+"name": "hello-world",
+"description": "hello world test app",
+"version": "0.0.1",
+"private": true,
+"dependencies": {
+"express": "3.4.4",
+"winston": "^0.8.0",
+"winston-papertrail": "^0.1.4"
+}
+}
